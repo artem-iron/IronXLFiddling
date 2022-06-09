7f07bc573e6f6fd0aa16e69322eb5215d858b32a
Artem Kolosov <artem@ironsoftware.com>
2022-06-09 15:33:33 +0700

    Add project files.

---
+++ TestNamespaceIssues/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using IronXL;
+using IronXL.Drawing.Images;
+//using NPOI.OpenXmlFormats.Spreadsheet;
+//using NPOI.SS.UserModel.Charts;
+//using NPOI.Util;
+
+namespace TestNamespaceIssues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkSheet ass = WorkBook.Load("C:\\Users\\artem\\Downloads\\TestData.xlsx").DefaultWorkSheet;
+
+            IImage pic = ass.Images[0];
+
+            if (pic != null)
+
+            {
+
+                Bitmap bitmap = pic.ToBitmap(); //It fails here with an exception, the details of which I sent you earlier.
+
+                bitmap.Save("C:\\Users\\artem\\Downloads\\ass.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+
+                bitmap.Dispose();
+
+            }
+        }
+    }
+}
